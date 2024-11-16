9091274a24b9a13cd90c42d40c71135dcd9b6c24
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-17 00:24:07 +0530

    register page and database with connector

---
+++ includes/register.inc.php
@@ -0,0 +1,35 @@
+<?php
+
+if(isset($_POST["register"])){
+    $name= $_POST["name"];
+    $email= $_POST["email"];
+    $password= $_POST["password"];
+    $cpassword= $_POST["cpassword"];
+    $image= $_POST["image"];
+
+
+    if(imptySet($name,$email,$password,$cpassword,$image)){
+        header("location: ../register.php?err=emptyError");
+    }
+    else if(invalidname($name)){
+       header("location: ../register.php?err=invalidName");
+    }
+    else if(invalidEmail($email)){
+       header("location: ../register.php?err=invalidEmail");
+    }
+    else if(invalidpassword($password)){
+       header("location: ../register.php?err=invalidpassword");
+    }
+    else if(matchpassword($password, $cpassword)){
+       header("location: ../register.php?err=donotmatchpass");
+    }
+    else if(availblenameEmail($name,$email)){
+       header("location: ../register.php?err=availbleNameOrEmail");
+    }
+    else{
+        echo "success";
+    }
+}
+
+
+?>
