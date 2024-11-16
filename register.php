1dbdea1835a58342114162c1651377ad1008a2aa
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-17 00:23:08 +0530

    create register page

---
+++ register.php
@@ -0,0 +1,34 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link rel="stylesheet" href="css/components.css">
+</head>
+<body>
+
+
+
+    <section class="form-container">
+      <form action="" method="POST" enctype="multipart/form-data">
+        <h1>Register</h1>
+         <input type="text" name="name" class="box" placeholder="enter your name" required> 
+         <br><br>
+         <input type="text" name="email"  class="box" placeholder="enter your email" required> 
+         <br><br>
+         <input type="password" name="password"  class="box" placeholder="enter your password" required> 
+         <br><br>
+         <input type="password" name="cpassword"  class="box" placeholder="enter your confirm password" required> 
+         <br><br>
+         <input type="file" name="image" class="box" required accept="image/jpg,image/jpeg,image/png">
+         <br><br>
+         <input type="submit" name="register" value="register now" class="btn" required>
+         <p>All ready you have account?<a href="login.php">Login</a></p>
+      </form> 
+    </section>
+</body>
+</html>
