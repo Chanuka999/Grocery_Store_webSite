c906fc776e31a8efb14401203ecf7c926ad39af9
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-19 16:46:05 +0530

    complete backend

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -8,10 +8,10 @@ if(isset($_POST["register"])){
     $email= $_POST["email"];
     $password= $_POST["password"];
     $cpassword= $_POST["cpassword"];
-    $image= $_POST["image"];
+    //$image= $_POST["image"];
 
 
-    if(imptySet($name,$email,$password,$cpassword,$image)){
+    if(imptySet($name,$email,$password,$cpassword,/*$image*/)){
         header("location: ../register.php?err=emptyError");
     }
     else if(invalidname($name)){
@@ -30,23 +30,30 @@ if(isset($_POST["register"])){
        header("location: ../register.php?err=availbleNameOrEmail");
     }
     else{
-       userRegister($name,$email,$password,$cpassword,$image);
+       userRegister($name,$email,$password,$cpassword,/*$image*/);
     }
 }else{
    header("location: ../register.php");
 }
 
 
-function userRegister($name,$email,$password,$cpassword,$image){
+function userRegister($name,$email,$password,$cpassword,/*image*/){
+  
+global $conn;
+   $hashpassword = password_hash($password, PASSWORD_DEFAULT);
 
-   $hashpassword = pashword_hash($password, PASSWORD_DEFAULT);
-
-   $sql = "INSERT INTO users (name,email,password,image) values(?,?,?,?);";
+   $sql = "INSERT INTO users (name,email,password) values(?,?,?);";
 
    $stmt = mysqli_stmt_init($conn);
 
    if(!mysqli_stmt_prepare($stmt,$sql)){
       header("location: ../register.php?err=failedstmt");
+   }else{
+     mysqli_stmt_bind_param($stmt, "sss" , $name,$email,$password );
+
+     mysqli_stmt_execute($stmt);
+     echo "successfully insert";
+     header("location: ../login.php?signup=successfull");
    }
    
 
