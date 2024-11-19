e485fd93310081d4127e3ca3811b4c1f52cebfb5
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-19 23:04:00 +0530

    login page created

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login page</title>
+    <link rel="stylesheet" href="css/components.css">
+</head>
+<body>
+  
+    
+    <div class="forms">
+        <form action="./includes/login.inc.php" method="POST" class="login">
+          <h2>Login</h2>
+          <input type="text" name="email" placeholder="Enter your email..">
+          <input type="text" name="password" placeholder="Enter your password..">
+          <div class="rem">
+          <input type="checkbox" name="re-check" id="re-check">
+          <label for="re-check">Remember Me</label>
+          </div>
+          <button type="submit" name="login-btn">Login</button>
+
+        </form>
+    </div>
+</body>
+</html>
