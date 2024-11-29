4f7ce5acd0992604459fbcae300c5ffd1eb70bad
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-29 13:02:54 +0530

    update

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -8,10 +8,14 @@ if(isset($_POST["register"])){
     $email= $_POST["email"];
     $password= $_POST["password"];
     $cpassword= $_POST["cpassword"];
-    //$image= $_POST["image"];
+    $imageName = $_FILES["image"]["name"];
+    $imageTmpName = $_FILES["image"]["tmp_name"];
+    $imageSize = $_FILES["image"]["size"];
+    $imageError = $_FILES["image"]["error"];
+    $imageType= $_FILES["image"]["type"];
 
 
-    if(imptySet($name,$email,$password,$cpassword,/*$image*/)){
+    if(imptySet($name,$email,$password,$cpassword,$image)){
         header("location: ../register.php?err=emptyError");
     }
     else if(invalidname($name)){
@@ -28,28 +32,30 @@ if(isset($_POST["register"])){
     }
     else if(availblEmail($email)){
        header("location: ../register.php?err=availbleNameOrEmail");
+    }else if(invalidImage($imageName,$imageTmpName,$imageSize,$imageError,$imageType)){
+      header("location: ../register.php?err=invalidImage");
     }
     else{
-       userRegister($name,$email,$password,$cpassword,/*$image*/);
+       userRegister($name,$email,$password,$cpassword,$imageName);
     }
 }else{
    header("location: ../register.php");
 }
 
 
-function userRegister($name,$email,$password,$cpassword,/*image*/){
+function userRegister($name,$email,$password,$cpassword,$imageName){
   
 global $conn;
    $hashpassword = password_hash($password, PASSWORD_DEFAULT);
 
-   $sql = "INSERT INTO users (name,email,password) values(?,?,?);";
+   $sql = "INSERT INTO users (name,email,password,image) values(?,?,?,?);";
 
    $stmt = mysqli_stmt_init($conn);
 
    if(!mysqli_stmt_prepare($stmt,$sql)){
       header("location: ../register.php?err=failedstmt");
    }else{
-     mysqli_stmt_bind_param($stmt, "sss" , $name,$email,$password );
+     mysqli_stmt_bind_param($stmt, "ssss" , $name,$email,$password,$imageName );
 
      mysqli_stmt_execute($stmt);
      echo "successfully insert";
