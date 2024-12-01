bda509996e6c180904b15c76395a3f1584827a7d
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-01 14:59:15 +0530

    update

---
+++ adminUpdateProfile.php
@@ -0,0 +1,17 @@
+<?php include "dbh.inc.php";   ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <?php include "admin_header.php";  ?>
+   <section class="udate-profile">
+     
+      <h1 class="title">update profile</h1>
+   </section>
+</body>
+</html>
